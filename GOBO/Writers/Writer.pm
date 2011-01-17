03e43b26914080323c2d68c191e750526b2de465
Siddhartha Basu <biosidd@gmail.com>
2011-01-17 16:09:22 -0600

    another commit after figuring out a way to output col17 in GAF

--- GOBO/Writers/Writer.pm
+++ GOBO/Writers/Writer.pm
@@ -3,51 +3,72 @@ use Moose;
 use GOBO::Graph;
 use FileHandle;
 
-has fh => (is=>'rw', isa=>'FileHandle', clearer=>'clear_fh', predicate=>'has_fh'); #, coerce=>1);
-has file => (is=>'rw', isa=>'Str', trigger => \&init_fh);
-has graph => (is=>'rw', isa=>'GOBO::Graph');
+has fh => (
+    is        => 'rw',
+    isa       => 'FileHandle',
+    clearer   => 'clear_fh',
+    predicate => 'has_fh'
+);    #, coerce=>1);
+has file => ( is => 'rw', isa => 'Str', trigger => \&init_fh );
+has graph => ( is => 'rw', isa => 'GOBO::Graph' );
 
 ## indexes to use for the different types of statement
-has 'statement_ix' => (is=>'rw', isa=>'Str', default=>sub { 'statements' } );
-has 'annotation_ix' => (is=>'rw', isa=>'Str', default=>sub { 'annotations' } );
-has 'edge_ix' => (is=>'rw', isa=>'Str', default=>sub { 'edges' } );
-has 'ontology_link_ix' => (is=>'rw', isa=>'Str', default=>sub { 'ontology_links' } );
+has 'statement_ix'  => ( is => 'rw', isa => 'Str', default => 'statements' );
+has 'annotation_ix' => ( is => 'rw', isa => 'Str', default => 'annotations' );
+has 'edge_ix'       => ( is => 'rw', isa => 'Str', default => 'edges' );
+has 'ontology_link_ix' =>
+    ( is => 'rw', isa => 'Str', default => 'ontology_links' );
 
-## set up output; 
+has 'header_data' => (
+    is      => 'rw',
+    isa     => 'ArrayRef',
+    traits  => [qw/Array/],
+    default => sub { [] },
+    handles => {
+        add_to_header     => 'push',
+        has_header_data   => 'count',
+        clear_header_data => 'clear',
+        get_header_data   => 'elements'
+
+    }
+);
+
+## set up output;
 sub BUILD {
-	my $self = shift;
-	$self->init_fh;
+    my $self = shift;
+    $self->init_fh;
 }
 
 sub create {
     my $proto = shift;
-    my %argh = @_;
-    my $fmt = $argh{format};
+    my %argh  = @_;
+    my $fmt   = $argh{format};
     if ($fmt) {
         my $pc;
-        if ($fmt eq 'obo') {
+        if ( $fmt eq 'obo' ) {
             $pc = 'GOBO::Writers::OBOWriter';
         }
-        elsif ($fmt eq 'oboxml') {
+        elsif ( $fmt eq 'oboxml' ) {
             $pc = 'GOBO::Writers::OBOXMLWriter';
         }
-	my $mod = $pc;
-	$mod =~ s/::/\//g;
-	require "$mod.pm";
-	# TODO
+        my $mod = $pc;
+        $mod =~ s/::/\//g;
+        require "$mod.pm";
+
+        # TODO
         return $pc->new(%argh);
     }
 }
 
 sub init_fh {
     my $self = shift;
-    if (!$self->fh) {
+    if ( !$self->fh ) {
         my $f = $self->file;
         my $fh;
         if ($f) {
             $fh = FileHandle->new(">$f");
         }
-        if (!$fh) {
+        if ( !$fh ) {
             $fh = FileHandle->new(">-");
         }
         $self->fh($fh);
@@ -59,7 +80,7 @@ sub xxxfile {
     if (@_) {
         my ($f) = @_;
         $self->{file} = $f;
-        $self->fh(FileHandle->new(">$f"));
+        $self->fh( FileHandle->new(">$f") );
     }
 
     return $self->{file};
@@ -69,40 +90,42 @@ sub write {
     my $self = shift;
 
     my %ah = @_;
-    if ($ah{graph}) {
-        $self->graph($ah{graph});
+    if ( $ah{graph} ) {
+        $self->graph( $ah{graph} );
     }
-#    $self->init_fh;
+
+    #    $self->init_fh;
     $self->write_header;
     $self->write_body;
 }
 
 sub printrow {
     my $self = shift;
-    my $row = shift;
-    my $fh = $self->fh;
-    print $fh join("\t",@$row),"\n";
+    my $row  = shift;
+    my $fh   = $self->fh;
+    print $fh join( "\t", @$row ), "\n";
     return;
 }
 
 sub print {
     my $self = shift;
-    my $fh = $self->fh;
+    my $fh   = $self->fh;
     print $fh @_;
     return;
 }
 
 sub println {
     my $self = shift;
-    my $fh = $self->fh;
-    print $fh @_,"\n";
+    my $fh   = $self->fh;
+    print $fh @_, "\n";
     return;
 }
 
 sub printf {
     my $self = shift;
-    my $fmt = shift;
-    my $fh = $self->fh;
+    my $fmt  = shift;
+    my $fh   = $self->fh;
+
     #if (grep {!defined($_)} @_) {
     #    confess "undefined value in @_";
     #}
@@ -112,11 +135,10 @@ sub printf {
 
 sub nl {
     my $self = shift;
-    my $fh = $self->fh;
+    my $fh   = $self->fh;
     print $fh "\n";
     return;
 }
-
 
 __PACKAGE__->meta->make_immutable;
 
